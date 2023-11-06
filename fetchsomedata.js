b03ebf9adb2f1260ed8926d51261e9fd46a924dd
macmkay <>
2023-11-06 15:24:21 +0700

    change fetchsomedata js file

--- fetchsomedata.js
+++ fetchsomedata.js
@@ -3,5 +3,5 @@ const endpoint = 'http://localhost:8055/channels?page=0';
 
 fetch('http://localhost:8055/channels?page=0')
   .then(response => response.json()) // Extract JSON data from the response
-  .then(data => console.log(data)) // Log the extracted data
+  .then(datae => console.log(datae)) // Log the extracted data
   .catch(error => console.log(error)); // Handle any errors that occur during the fetch
